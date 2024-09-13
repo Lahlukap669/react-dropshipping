9364dd64507d05372fabbf9a662ad92ec1dc7762
Lahlukap669 <luka1.lah@gmail.com>
2024-09-13 14:58:29 +0200

    added signin component and firebase utils and lib

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,46 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import {getFirestore, doc, getDoc, setDoc}  from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA4EYfAKJeiZEtTIbjX6FWNPAb5YuyRotc",
+    authDomain: "dropshipping-db-624df.firebaseapp.com",
+    projectId: "dropshipping-db-624df",
+    storageBucket: "dropshipping-db-624df.appspot.com",
+    messagingSenderId: "132839877984",
+    appId: "1:132839877984:web:5e83f997fe8fa728544205"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+
+export const auth = getAuth(firebaseApp);
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore(firebaseApp);
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+    // if user data exists
+
+    //if user does not exist
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.error('Error creating user', error.message);
+        }
+    }
+    return userDocRef;
+}
