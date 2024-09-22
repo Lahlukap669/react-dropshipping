c1dce8d1757f3b22515bc390f65728624169c0af
Lahlukap669 <luka1.lah@gmail.com>
2024-09-22 10:47:16 +0200

    shop routes and styles finished

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,14 @@
+import './directory-item.styles.scss';
+
+const DirectoryItem = ({title, imageUrl}) => {
+    return (
+        <div className='directory-item-container'>
+            <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}}/>
+            <div className='directory-item-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+            </div>
+        </div>
+    );
+}
+export default DirectoryItem;
